2d9195ff83adcbce40109c7554b279f52bbd0f7d
antoine jubin <antoine.jubin@depinfonancy.net>
2019-05-03 15:56:15 +0200

    Updates

---
+++ precache-manifest.7ae50d953f9036be7977e8d63eafcebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e425150331fa1dfc321557598c54ce5",
+    "url": "/algolia-quickstart/index.html"
+  },
+  {
+    "revision": "52d725191e56057c62ec",
+    "url": "/algolia-quickstart/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/algolia-quickstart/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "52d725191e56057c62ec",
+    "url": "/algolia-quickstart/static/js/main.8353e75a.chunk.js"
+  },
+  {
+    "revision": "f6abd79d89dce74ab0c7",
+    "url": "/algolia-quickstart/static/js/runtime~main.5a1c7234.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/algolia-quickstart/static/media/logo.5d5d9eef.svg"
+  }
+]);
